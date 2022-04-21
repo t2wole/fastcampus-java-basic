8bb581c47e4ecd60429bd5f2b398faaac5e62e61
t2wole <t2wole@gmail.com>
2022-04-21 23:16:37 +0900

    ch02

--- src/Chapter02_09/studentTest.java
+++ src/Chapter02_09/studentTest.java
@@ -5,8 +5,15 @@ public class studentTest {
 	public static void main(String[] args) {
 		
 		student studentLee = new student(100, "Lee");
+		studentLee.setKoreaSubject("국어", 100);
+		studentLee.setMathSubject("수학", 90);
 		
+		student studentKim = new student(109, "Kim");
+		studentKim.setKoreaSubject("국어", 80);
+		studentKim.setMathSubject("수학", 70);
 		
+		studentLee.showStudentScore();
+		studentKim.showStudentScore();
 	}
 
 }
